d9a818c313daf2e3ef24cc720a0d6bc9ed2ab263
hs <henri@mobalean.com>
2015-04-30 16:55:42 +0900

    Add call arguments handling

--- dht11_lcd.py
+++ dht11_lcd.py
@@ -14,6 +14,19 @@ import Adafruit_DHT
 from time import sleep
 
 
+## Handle args
+sensor_args = { '11': Adafruit_DHT.DHT11,
+				'22': Adafruit_DHT.DHT22,
+				'2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+	sensor = sensor_args[sys.argv[1]]
+	pin = sys.argv[2]
+else:
+	print 'usage: sudo ./dht11_lcd.py [11|22|2302] GPIOpin#'
+	print 'example: sudo ./dht11_lcd.py 2302 4 - Read from an AM2302 connected to GPIO #4'
+	sys.exit(1)
+
+
 class Generic_LCD:
 
     # commands
